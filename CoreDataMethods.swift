c89662d4157494b1bf5060acaceb1f4bfe2fef37
Rati <rati@Krishnas-iMac.local>
2017-06-15 11:24:09 +0530

    changes

--- CoreDataMethods.swift
+++ CoreDataMethods.swift
@@ -8,7 +8,7 @@
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "Tin_Jobs")
+        let container = NSPersistentContainer(name: "") //Enter xcdatamodeld name
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
